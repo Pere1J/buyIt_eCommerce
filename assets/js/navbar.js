7d3229f984bf90bce88f466442084affd48b6ed0
alejandro <alexcs9230@gmail.com>
2022-12-19 10:46:17 +0100

    added navbar icons

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,5 @@
 function showMenu(){
     document.querySelector('.navigation').classList.toggle('active');
-    document.querySelector('.menu').classList.toggle('hide');
+    document.querySelector('.fa-solid fa-bars').classList.toggle('hide');
     document.querySelector('.close').classList.toggle('show');
 }
