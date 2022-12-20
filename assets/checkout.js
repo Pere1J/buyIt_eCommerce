130004dabe11c278dcd44bda34e82e6096364f06
GustavoArdaya <gustavo.ardaya@gmail.com>
2022-12-20 13:08:26 +0100

    added functionality to checkout box buttons

--- assets/checkout.js
+++ assets/checkout.js
@@ -7,23 +7,31 @@ let a = 0;
 function addItem() {
   if (a < 10) {
     a++;
-    console.log(a);
+    //console.log(a);
+    
   } else {
     a = 10;
-    console.log(a);
+    //console.log(a);
   }
+  renderNumber();
 }
 
 function subsItem() {
   if (a > 0) {
     a--;
-    console.log(a);
+    //console.log(a);
   } else {
     a = 0;
-    console.log(a);
+    //console.log(a);
   }
+  renderNumber();
 }
 
+function renderNumber() {
+    let totalItems = document.querySelector("#quantity_number");
+    totalItems.innerHTML = a.toString();
+    //console.log(totalItems);
+}
 //document.getElementById(".quantity_number span").innerHTML = a;
-document.getElementsByClassName(".quantity_number span").innerHTML =
-  a.toString();
+
+renderNumber();
