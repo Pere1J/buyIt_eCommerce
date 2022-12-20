5053780aa2da34a833df8fbebbf8860320481455
GustavoArdaya <gustavo.ardaya@gmail.com>
2022-12-20 12:36:18 +0100

    added dividing lines to quantity buttons

--- assets/checkout.js
+++ assets/checkout.js
@@ -1,30 +1,29 @@
-const plus = document.querySelector(".plus_button"),
-minus = document.querySelector(".minus_button"),
-num = document.querySelector(".quantity_number");
+// const plus = document.querySelector(".plus_button"),
+// minus = document.querySelector(".minus_button"),
+// num = document.querySelector(".quantity_number");
 
 let a = 0;
 
 function addItem() {
-    if (a < 10) {
-        a++;
+  if (a < 10) {
+    a++;
     console.log(a);
-    } else {
-        a = 10;
-        console.log(a);
-    }
-    
-    
-    }
+  } else {
+    a = 10;
+    console.log(a);
+  }
+}
 
 function subsItem() {
-    if (a > 0) {
-        a--
+  if (a > 0) {
+    a--;
     console.log(a);
-    } else {
-        a = 0;
-        console.log(a);
-    }
-
-    
-    
+  } else {
+    a = 0;
+    console.log(a);
+  }
 }
+
+//document.getElementById(".quantity_number span").innerHTML = a;
+document.getElementsByClassName(".quantity_number span").innerHTML =
+  a.toString();
